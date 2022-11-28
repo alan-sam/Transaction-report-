11564d78dbef09af137f4f7697fb17a12f793019
Alan Sam <66976535+alan-sam@users.noreply.github.com>
2022-11-28 16:17:01 -0600

    Add files via upload

---
+++ src/transaction/jOptionPane.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transaction;
+
+/**
+ *
+ * @author alans
+ */
+class jOptionPane {
+
+    static int ERROR_MESSAGE;
+    
+}
